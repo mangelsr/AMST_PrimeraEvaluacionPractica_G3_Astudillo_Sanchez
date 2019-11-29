70e2c77985fc14f775259291d07104d595e9c420
Miguel Sanchez <mangelsr@live.com>
2019-11-29 08:59:52 -0500

    [INIT]

---
+++ app/src/main/java/amst/g1/amst_primeraevaluacion_g3_astudillo_sanchez/MainActivity.java
@@ -0,0 +1,14 @@
+package amst.g1.amst_primeraevaluacion_g3_astudillo_sanchez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
